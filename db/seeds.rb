28a883f730cff47c7546a3658cdb972f7ede571c
Andrey <shmatuan@gmail.com>
2016-02-03 15:02:40 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 Ferry.create(name: "BigFoot", typeship: "large")
 Ferry.create(name: "Lion", typeship: "small")
 
-Timetable.create(date: DateTime.new(2016,2,5,10,0), ferry_id: 1, direction: 'Maiami')
-Timetable.create(date: DateTime.new(2016,2,5,17,0), ferry_id: 2, direction: 'Cuba')
-Timetable.create(date: DateTime.new(2016,2,7,15,0), ferry_id: 2, direction: 'Maiami')
-Timetable.create(date: DateTime.new(2016,2,7,12,0), ferry_id: 1, direction: 'Cuba')
+(3..28).each do |i|
+  if i%2 == 0
+    Timetable.create(date: DateTime.new(2016,2,i,10,0), ferry_id: 1, direction: 'Maiami')
+    Timetable.create(date: DateTime.new(2016,2,i,17,0), ferry_id: 2, direction: 'Cuba')
+  else
+    Timetable.create(date: DateTime.new(2016,2,i,15,0), ferry_id: 2, direction: 'Maiami')
+    Timetable.create(date: DateTime.new(2016,2,i,12,0), ferry_id: 1, direction: 'Cuba')
+  end
+end
+
 
 (0..6).each do |i|
-  Place.create(ferry_id: 1, name: "AA#{i+1}", price: 3)
+  Place.create(ferry_id: 1, name: "AA#{i+1}", price: 4)
   Place.create(ferry_id: 1, name: "AB#{i+1}", price: 3)
   Place.create(ferry_id: 1, name: "AC#{i+1}", price: 3)
   Place.create(ferry_id: 1, name: "BA#{i+1}", price: 3)
