f5e09c15547a9252aadbf43a07c6e3b41cfdc445
Andrey <shmatuan@gmail.com>
2016-02-04 13:41:54 +0200

    css

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,31 +12,38 @@ Ferry.create(name: "Lion", typeship: "small")
 end
 
 
-(0..6).each do |i|
-  Place.create(ferry_id: 1, name: "AA#{i+1}", price: 4)
-  Place.create(ferry_id: 1, name: "AB#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "AC#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "BA#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "BB#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "BC#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "DA#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "DB#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "DC#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "EA#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "EB#{i+1}", price: 3)
-  Place.create(ferry_id: 1, name: "EC#{i+1}", price: 3)
-end
-(0..16).each do |i|
-  Place.create(ferry_id: 1, name: "CA#{i+1}", price: 2)
-  Place.create(ferry_id: 1, name: "CB#{i+1}", price: 2)
-  Place.create(ferry_id: 1, name: "CC#{i+1}", price: 2)
-end
+create_places('large', 1)
+create_places('small', 2)
 
-(0..16).each do |i|
-  Place.create(ferry_id: 2, name: "AA#{i+1}", price: 5)
-  Place.create(ferry_id: 2, name: "AB#{i+1}", price: 5)
-  Place.create(ferry_id: 2, name: "AC#{i+1}", price: 5)
-  Place.create(ferry_id: 2, name: "BA#{i+1}", price: 5)
-  Place.create(ferry_id: 2, name: "BB#{i+1}", price: 5)
-  Place.create(ferry_id: 2, name: "BC#{i+1}", price: 5)
+def create_places(type, id)
+  if type == 'small'
+    (0..16).each do |i|
+      Place.create(ferry_id: id, name: "AA#{i+1}", price: 5)
+      Place.create(ferry_id: id, name: "AB#{i+1}", price: 5)
+      Place.create(ferry_id: id, name: "AC#{i+1}", price: 5)
+      Place.create(ferry_id: id, name: "BA#{i+1}", price: 5)
+      Place.create(ferry_id: id, name: "BB#{i+1}", price: 5)
+      Place.create(ferry_id: id, name: "BC#{i+1}", price: 5)
+    end
+  elsif type == 'large'
+    (0..6).each do |i|
+      Place.create(ferry_id: id, name: "AA#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "AB#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "AC#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "BA#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "BB#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "BC#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "DA#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "DB#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "DC#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "EA#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "EB#{i+1}", price: 3)
+      Place.create(ferry_id: id, name: "EC#{i+1}", price: 3)
+    end
+    (0..16).each do |i|
+      Place.create(ferry_id: id, name: "CA#{i+1}", price: 2)
+      Place.create(ferry_id: id, name: "CB#{i+1}", price: 2)
+      Place.create(ferry_id: id, name: "CC#{i+1}", price: 2)
+    end
+  end
 end
