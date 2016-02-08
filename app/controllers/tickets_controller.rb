32e8eedd21f8c31a07998640ad69861469c2c13a
Andrey <shmatuan@gmail.com>
2016-02-08 10:16:44 +0200

    paypal1

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,6 +13,24 @@ class TicketsController < ApplicationController
     end
     date = @timetable.date
     @ft = Timetable.where(date: date.beginning_of_day..date.end_of_day).order('date')
+
+    redirect_to paypal_url("")
   end
+
+  def paypal_url(return_path)
+    values = {
+        business: "shmatuan-facilitator@gmail.com",
+        cmd: "_xclick",
+        upload: 1,
+        return: "#{Rails.application.secrets.app_host}#{return_path}",
+        invoice: id,
+        amount: price,
+        item_name: "Ferry tickets",
+        item_number: number,
+        quantity: '1'
+    }
+    "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+  end
+
 
 end
