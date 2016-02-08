c822dd1c93d3f751327c152a06b8eaa1517b7942
Andrey <shmatuan@gmail.com>
2016-02-08 13:09:21 +0200

    paypal values

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,41 +4,38 @@ class TicketsController < ApplicationController
   end
   
   def pay
+
+    paypal_values = {items_name: '', id: '', price: 0, number: ''}
+
     @timetable = Timetable.find(params[:timetable])
+    paypal_values[:id] = "#{@timetable.id}-#{params[:places]}"
+    paypal_values[:number] = params[:places]
     @sheep = @timetable.ferry
     places = params[:places].split('-')
     places.each do |place_name|
       place = Place.find_by(name: place_name, ferry_id: @sheep)
       Ticket.create(place_id: place.id,  timetable_id: @timetable.id)
+      paypal_values[:price] += place.price
+      paypal_values[:items_name] += "#{place.name}#{', ' if place_name != places.last}"
     end
-
-    @url = paypal_url("")
-    p 'url'*5
-    p @url
+    @url = paypal_url("", paypal_values)
     respond_to do |format|
       format.js
     end
   end
 
 
-  def paypal_url(return_path)
-
-    items_name = 'Ferry tickets'
-    id = 4
-    price = 125
-    number = 5
-    quantity = 1
-
+  def paypal_url(return_path, paypal_values)
     values = {
         business: 'shmatuan-facilitator@gmail.com',
         cmd: '_xclick',
         upload: 1,
         return: "#{Rails.application.secrets.app_host}#{return_path}",
-        invoice: id,
-        amount: price,
-        item_name: 'Ferry tickets' + items_name,
-        item_number: number,
-        quantity: quantity
+        invoice: paypal_values[:id],
+        amount: paypal_values[:price],
+        item_name: 'Ferry tickets ' + paypal_values[:items_name],
+        item_number: paypal_values[:number],
+        quantity: 1
     }
     "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
   end
